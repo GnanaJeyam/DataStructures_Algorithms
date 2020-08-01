fab84b05553e7bcd358f06613b5753a023aa5856
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-01 14:55:40 +0530

    Geeksforgeeks -> Convert a Tree to Mirror Tree.

---
+++ Trees/MirrorTree.java
@@ -0,0 +1,15 @@
+/**
+ * This is Function problem to convert the tree to mirror tree.
+ */
+class MirrorTree
+{
+    void mirror(Node node)
+    {
+        if(node == null) return;
+        mirror(node.left);
+        mirror(node.right);
+        Node t = node.left;
+        node.left = node.right;
+        node.right = t;
+    }
+}
