4704f6f83b51ab43dbaa9efd55f33a15220b656b
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-01 15:13:47 +0530

    GeeksforGeeks -> Print the zig zag traversal of tree.

---
+++ Trees/ZigZagTreeTraversal.java
@@ -0,0 +1,46 @@
+// Print the zig zag traversal of tree.
+class ZigZagTreeTraversal
+{
+    // return an array containing the zig zag level order traversal of the given tree
+	ArrayList<Integer> zigZagTraversal(Node root)
+	{
+	    
+		Queue<Node> queue = new LinkedList<>();
+		queue.add(root);
+		boolean flag = true;
+		ArrayList<Integer> ap = new ArrayList<>();
+
+		while(!queue.isEmpty()) {
+			List<Integer> res = new ArrayList<>();
+			int c = queue.size();
+			for(int i=0; i<c; i++) {
+				Node t = queue.poll();
+				if(t.left != null) {
+					queue.add(t.left);
+				}
+				if(t.right != null) {
+					queue.add(t.right);
+				}
+				res.add(t.data);
+			}
+			
+			printInt(res, flag,ap);
+			flag = !flag;
+		}
+		
+		return ap;
+	
+	}
+	
+	static void printInt(List<Integer> ls , boolean flag,ArrayList<Integer> ap) {
+		if(flag) {
+			for(int i=0; i<ls.size(); i++) {
+				ap.add(ls.get(i));
+			}
+		}else {
+			for(int i=ls.size()-1; i>=0; i--) {
+				ap.add(ls.get(i));
+			}
+		}
+	}
+}
