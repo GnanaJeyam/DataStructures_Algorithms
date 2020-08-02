d8f2e11e8d06fdc01b371968264fa09d038d07c9
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-02 17:47:23 +0530

    GeeksforGeeks -> Print all nodes that don't have sibling.

---
+++ Trees/FindSiblingInBtree.java
@@ -0,0 +1,39 @@
+/**
+ * This is the functional problem to find the siblings in the binary tree.
+ * https://practice.geeksforgeeks.org/problems/print-all-nodes-that-dont-have-sibling/1
+ */
+class FindSiblingInBtree
+{   
+    private ArrayList<Integer> nodes = new ArrayList<>();
+	void printSibling(Node node)
+	{
+	    findSibling(node);
+	    
+	    if(nodes.isEmpty()){
+	        System.out.print(-1);
+	        return;
+	    }
+	    nodes
+	      .stream()
+          .sorted()
+          .forEach(e -> System.out.print(e+" "));
+        
+	}
+	
+	private void findSibling(Node node) {
+	    
+	    if(node == null) {
+	        return;
+	    }
+	    
+	    if(node.left != null && node.right == null) {
+	        nodes.add(node.left.data);
+	    } else if(node.right != null && node.left == null) {
+	        nodes.add(node.right.data);
+	    }
+	    
+	    findSibling(node.left);
+	    findSibling(node.right);
+	    
+	}
+}
