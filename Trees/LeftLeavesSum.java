3d43eea3e34a9c5b049ac21ed452d5f05392ce86
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-02 17:12:24 +0530

    Geeksforgeeks -> Sum of Left Leaf Nodes.

---
+++ Trees/LeftLeavesSum.java
@@ -0,0 +1,25 @@
+/**
+ * This is functional problem to add left leaves sum in binary tree.
+ */
+class LeftLeavesSum {
+    private ArrayList<Integer>a=new ArrayList<>();
+    public int leftLeavesSum(Node root) 
+    { 
+       dfs(root);
+       int cnt=0;
+       for(int i:a)cnt+=i;
+       return cnt;
+    
+    } 
+    public void dfs(Node n){
+        if(n==null)return ;
+        
+        if(n.left!=null &&(n.left.left==null&&n.left.right==null))
+        a.add(n.left.data);
+        
+        dfs(n.right);
+        dfs(n.left);
+
+    }
+
+}
