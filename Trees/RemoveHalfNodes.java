949d7cb50e8696ee87982ff9fc7c5b474d459c2f
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-02 13:20:37 +0530

    GeeksforGeeks -> Given A binary Tree. Your task is to remove all the half nodes (which has only one child).

---
+++ Trees/RemoveHalfNodes.java
@@ -0,0 +1,25 @@
+/**
+ * This is functional problem. Given A binary Tree. Your task is to remove all the half nodes (which has only one child).
+ */
+class RemoveHalfNodes
+{
+    // Return the root of the modified tree after removing all the half nodes.
+    public static Node RemoveHalfNodes(Node root)
+    {
+        if(root == null) return root;
+    
+        root.left = RemoveHalfNodes(root.left);
+        root.right = RemoveHalfNodes(root.right);
+        
+        if(root.left == null && root.right != null){
+            return root.right;
+        }
+        
+        if(root.left != null && root.right == null){
+            return root.left;
+        }
+        
+        
+        return root;
+    }
+}
