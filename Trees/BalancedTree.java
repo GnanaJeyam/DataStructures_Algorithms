ba333611b0f5d2bfa3b80eec3e9af392b34020b6
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-02 13:59:27 +0530

    GeeksforGeeks -> Given a binary tree, find if it is height balanced or not.

---
+++ Trees/BalancedTree.java
@@ -0,0 +1,27 @@
+/**
+ * This is functional problem to check whether the tree is balanced or not.
+ * The tree is balanced if its left-subtree
+ */
+class BalancedTree
+{
+    private boolean flag = true;
+     /* This function should return tree if passed  tree 
+     is balanced, else false. */
+    boolean isBalanced(Node root)
+    {
+        if(root == null) return true;
+        
+        getMaxHeight(root);
+        return flag;
+    }
+    
+    private int getMaxHeight(Node root) {
+        if(root == null) return 0;
+        int a = getMaxHeight(root.left);
+        int b = getMaxHeight(root.right);
+        if(Math.abs(a - b) > 1) {
+            flag = false;
+        }
+        return 1 + Math.max(a, b);
+    }
+}
