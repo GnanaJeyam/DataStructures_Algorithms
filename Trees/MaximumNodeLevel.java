9e6554824da9415b65ee0fc7ac714d9edd5baa7d
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-02 13:22:36 +0530

    GeeksforGeeks -> Given a Binary tree. Find the level in binary tree which has the maximum number of nodes.

---
+++ Trees/MaximumNodeLevel.java
@@ -0,0 +1,42 @@
+/**
+ * Given a Binary tree. Find the level in binary tree which has the maximum number of nodes.
+ */
+class MaximumNodeLevel
+{
+    // Return the level (0-indexed) with maximum number of nodes.
+    public static int maxNodeLevel(Node root)
+    {
+        if(root == null) 
+            return 0;
+            
+        Queue<Node> queue = new LinkedList<>();
+        
+        queue.add(root);
+        int maxLevel = 0;
+        int maxCount = -1;
+        int level = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            
+            for(int i=0; i<size; i++) {
+                Node temp = queue.poll();
+                
+                if (temp.left != null) {
+                    queue.add(temp.left);
+                }
+                
+                if (temp.right != null) {
+                    queue.add(temp.right);
+                }
+            }
+            
+            if(size > maxCount){
+                maxCount = size;
+                maxLevel = level;
+            }
+            level++;
+        }    
+        
+        return maxLevel;
+    }
+}
