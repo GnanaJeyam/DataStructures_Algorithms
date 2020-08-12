ba1d3fed8ee2d91da22f8641c60ce7af526922ab
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-12 17:07:47 +0530

    Geeksforgeeks -> Print BST elements in given range.

---
+++ Trees/BSTInRange.java
@@ -0,0 +1,36 @@
+/**
+ * This is a functional problem to Print BST elements in given range.
+ * https://practice.geeksforgeeks.org/problems/print-bst-elements-in-given-range/1
+ */
+class BSTInRange
+{   
+	public static ArrayList<Integer> printNearNodes(Node r, int low, int high)
+	{
+	    ArrayList<Integer> array = new ArrayList<>();
+	    
+	    return findNearNodes(r, low, high, array);
+    }
+    
+    private static ArrayList<Integer> findNearNodes(Node r, int low, int high, ArrayList<Integer> array) {
+        
+	    if(r == null) return array;
+	    
+	    int data = r.data;
+	    
+	    if (data > low) {
+            findNearNodes(r.left, low, high, array);	        
+	    }
+	    
+	    if(data >= low && data <= high){
+	        array.add(data);
+	    }
+	    
+	    if (data <= high) {
+	        findNearNodes(r.right, low, high, array);
+	    }     
+	    
+	    return array;
+    
+    }
+    
+}
