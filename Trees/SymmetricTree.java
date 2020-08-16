fc157c95ed5244167c09beef9379528e3f4f8900
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-16 13:30:49 +0530

    GeeksforGeeks -> Check whether it is Symmetric or not, i.e. whether the binary tree is a Mirror image of itself or not.

---
+++ Trees/SymmetricTree.java
@@ -0,0 +1,27 @@
+/**
+ * This is the function problem to find the whether the binary tree is a Mirror image of itself or not.
+ * https://practice.geeksforgeeks.org/problems/symmetric-tree/1
+ */
+class SymmetricTree
+{
+    // return true/false denoting whether the tree is Symmetric or not
+    public static boolean isSymmetric(Node root)
+    {
+        if(root == null) return true;
+        
+        return symmetric(root.left, root.right);
+    }
+    
+    static boolean symmetric(Node l, Node r){
+        
+        if(l == null && r == null) return true;
+        
+        if(l == null || r == null) return false;
+        
+        if(l.data != r.data) return false;
+        
+        return symmetric(l.left, r.right) && symmetric(l.right, r.left);
+        
+    }
+    
+}
