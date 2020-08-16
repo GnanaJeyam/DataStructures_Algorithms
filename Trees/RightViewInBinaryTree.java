cb1c78fdc43055e9c8b9679a3d33c6436ab5d3ac
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-16 17:42:00 +0530

    GeeksforGeeks -> Right View of Binary Tree .

---
+++ Trees/RightViewInBinaryTree.java
@@ -0,0 +1,30 @@
+/**
+ * This is function problem to print the right view of the binary tree.
+ * https://practice.geeksforgeeks.org/problems/right-view-of-binary-tree/1
+ */
+class RightViewInBinaryTree {
+    void rightView(Node root) {
+      if(root == null) return;
+      
+      Queue<Node> queue = new LinkedList<Node>();
+      queue.add(root);
+      
+      while(!queue.isEmpty()){
+          int size = queue.size();
+          
+          while(size-- > 0) {
+              Node t = queue.poll();
+              if (size == 0) { // Points the last element.
+                System.out.print(t.data+" ");    
+              }
+              if(t.left != null){
+                  queue.add(t.left);
+              }
+              if(t.right != null){
+                  queue.add(t.right);
+              }
+          }
+      }
+      
+    }
+}
