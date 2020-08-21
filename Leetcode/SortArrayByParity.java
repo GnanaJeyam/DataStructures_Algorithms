b1082c4f12113ab1fb42c9e36861f7f6240c5726
Jeyam-GJ <gkannakumar@ntst.com>
2020-08-21 15:23:32 +0530

    Leetcode -> Sort Array By Parity.

---
+++ Leetcode/SortArrayByParity.java
@@ -0,0 +1,23 @@
+/**
+ * This is the Leetcode problem to sort array by parity
+ * https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/551/week-3-august-15th-august-21st/3431
+ */
+class SortArrayByParity {
+    public int[] sortArrayByParity(int[] a) {
+        int s = 0, e = a.length - 1;
+        
+        while(s < e) {
+            if(a[s] % 2 != 0 && a[e] %2 == 0){
+                int t = a[s];
+                a[s++] = a[e];
+                a[e--] = t;
+            }else if(a[s] % 2 == 0) {
+                s++;
+            }else {
+                e--;
+            }
+        }
+        
+        return a;
+    }
+}
