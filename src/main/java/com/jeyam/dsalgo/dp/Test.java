fffa804511f0e79bfa749febf58cb0bc0fde961c
Gnanajeyam <gnanajeyam95@gmail.com>
2024-08-28 21:20:48 +0530

    Updated gradle-wrapper.properties | Jeyam

---
+++ src/main/java/com/jeyam/dsalgo/dp/Test.java
@@ -0,0 +1,13 @@
+package com.jeyam.dsalgo.dp;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 5;
+        System.out.println("sd " + a + b);
+
+    }
+}
